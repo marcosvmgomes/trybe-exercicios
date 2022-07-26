816754b1333bd48a86c58e641a3238a568a1cebc
Marcos Gomes <markimmarques@hotmail.com>
2022-07-26 12:06:36 -0300

    Exercício 7

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/scripts.js
@@ -1,9 +1,17 @@
-const number = -2;
+const grade = 55;
 
-if(number > 0) {
-    console.log('positive')
-} else if(number < 0) {
-    console.log('negative')
+if (grade < 0 || grade > 100) {
+  console.log("Erro, nota incorreta!");
+} else if (grade >= 90) {
+  console.log("A");
+} else if (grade >= 80) {
+  console.log("B");
+} else if (grade >= 70) {
+  console.log("C");
+} else if (grade >= 60) {
+  console.log("D");
+} else if (grade >= 50) {
+  console.log("E");
 } else {
-    console.log('zero')
+  console.log("F");
 }
