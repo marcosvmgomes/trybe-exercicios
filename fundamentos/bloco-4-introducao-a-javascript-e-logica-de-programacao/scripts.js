a2ab835cec54ef62e6d594b0ec603508817dbf15
Marcos Gomes <markimmarques@hotmail.com>
2022-07-26 11:56:11 -0300

    Exercício 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/scripts.js
@@ -1,8 +1,11 @@
 const a = 13;
 const b = 7;
+const c = 15;
 
-if (a > b) {
-    console.log("'a' é maior que 'b'");
-  } else {
-    console.log("'b' é maior que 'a'");
-  };
+if (a > b && a > c) {
+    console.log('O número maior é: ' + a + '(a)')
+} else if (b > a && b > c) {
+    console.log('O número maior é: ' + b + '(b)')
+} else {
+    console.log('O número maior é: ' + c + '(c)')
+}
